3232a8b0bf93a209c6ec8f80cdd247d989286588
Maxi <maxi.gatto96@gmail.com>
2024-09-28 18:11:11 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+import { AppModule } from './app.module';
+import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const logger = new Logger('Main Orders');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
+    },
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen();
+  logger.log(`Orders microservice running on port ${envs.port}`);
+}
+bootstrap();
---
+++ src/orders/orders.service.ts
@@ -0,0 +1,161 @@
+import {
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+import { CreateOrderDto } from './dto/create-order.dto';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { OrderPaginationDto } from './dto/oder-pagination.dto';
+import { ChangeOrderStatusDto } from './dto/change-order-status.dto';
+import { NATS_SERVICE } from 'src/config';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class OrdersService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('OrdersService');
+
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {
+    super();
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connected');
+  }
+  async create(createOrderDto: CreateOrderDto) {
+    try {
+      const productIds = createOrderDto.items.map((item) => item.productId);
+      const products = await firstValueFrom(
+        this.client.send({ cmd: 'validate_products' }, productIds),
+      );
+
+      const totalAmount = createOrderDto.items.reduce((acc, orderItem) => {
+        const price = products.find(
+          (product) => product.id === orderItem.productId,
+        ).price;
+
+        return price * orderItem.quantity + acc;
+      }, 0);
+
+      const totalItems = createOrderDto.items.reduce((acc, orderItem) => {
+        return acc + orderItem.quantity;
+      }, 0);
+
+      const order = await this.order.create({
+        data: {
+          totalAmount,
+          totalItems,
+          OrderItem: {
+            createMany: {
+              data: createOrderDto.items.map(({ productId, quantity }) => ({
+                productId,
+                quantity,
+                price: products.find((product) => product.id === productId)
+                  .price,
+              })),
+            },
+          },
+        },
+        include: {
+          OrderItem: {
+            select: {
+              price: true,
+              quantity: true,
+              productId: true,
+            },
+          },
+        },
+      });
+
+      return {
+        ...order,
+        OrderItem: order.OrderItem.map((orderItem) => ({
+          ...orderItem,
+          name: products.find((product) => product.id === orderItem.productId)
+            .name,
+        })),
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Products not found',
+      });
+    }
+  }
+
+  async findAll({ status, page, limit }: OrderPaginationDto) {
+    const totalPages = await this.order.count({
+      where: {
+        status,
+      },
+    });
+
+    return {
+      data: await this.order.findMany({
+        where: {
+          status,
+        },
+        skip: (page - 1) * limit,
+        take: limit,
+      }),
+      meta: {
+        total: totalPages,
+        page,
+        lastPage: Math.ceil(totalPages / limit),
+      },
+    };
+  }
+
+  async findOne(id: string) {
+    const order = await this.order.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        OrderItem: {
+          select: {
+            price: true,
+            quantity: true,
+            productId: true,
+          },
+        },
+      },
+    });
+    if (!order)
+      throw new RpcException({
+        status: HttpStatus.NOT_FOUND,
+        message: `Order #${id} not found`,
+      });
+
+    const productIds = order.OrderItem.map((orderItem) => orderItem.productId);
+    const products = await firstValueFrom(
+      this.client.send({ cmd: 'validate_products' }, productIds),
+    );
+
+    return {
+      ...order,
+      OrderItem: order.OrderItem.map((orderItem) => ({
+        ...orderItem,
+        name: products.find((product) => product.id === orderItem.productId)
+          .name,
+      })),
+    };
+  }
+
+  async changeStatus({ id, status }: ChangeOrderStatusDto) {
+    const order = await this.findOne(id);
+    if (order.status === status) return order;
+    return this.order.update({
+      where: {
+        id,
+      },
+      data: {
+        status,
+      },
+    });
+  }
+}
